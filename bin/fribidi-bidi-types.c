0548d9cc1f6794ac714b66f8d870aeaa0d14da4a
behdad <behdad>
2005-09-04 16:55:12 +0000

    Use capital hex digits.

--- bin/fribidi-bidi-types.c
+++ bin/fribidi-bidi-types.c
@@ -10,7 +10,7 @@ main (
   FriBidiChar c;
 
   for (c = 0; c < FRIBIDI_UNICODE_CHARS; c++)
-    printf ("0x%04lx	%s\n", (long) c,
+    printf ("0x%04lX	%s\n", (long) c,
 	    fribidi_get_bidi_type_name (fribidi_get_bidi_type (c)));
 
   return 0;
