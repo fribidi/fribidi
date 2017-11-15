3325a3fb93082ffd6c54da8e49b2ac82a2940d6d
Dov Grobgeld <dov.grobgeld@gmail.com>
2017-11-15 22:08:59 +0100

    Updated licensing inline documentation.

--- gen.tab/packtab.h
+++ gen.tab/packtab.h
@@ -16,7 +16,7 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
  * Boston, MA 02110-1301, USA  
  * 
- * For licensing issues, contact <fwpg@sharif.edu>. 
+ * For licensing issues, contact <fribidi.license@gmail.com>.
  */
 
 #ifndef PACKTAB_H
