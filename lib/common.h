fribidi/fribidi

commit a7cf35b510dca601421ae36b14446bc800c44731

Drop the rest of #if FOO+0
https://bugzilla.gnome.org/show_bug.cgi?id=792481#c12

lib/common.h
@@ -26,7 +26,7 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-#if HAVE_CONFIG_H+0
+#ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 
@@ -72,13 +72,13 @@
 # endif	/* !fribidi_free */
 #endif /* fribidi_malloc */
 
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 
@@ -115,7 +115,7 @@
 # define FRIBIDI_EMPTY_STMT FRIBIDI_BEGIN_STMT (void) 0; FRIBIDI_END_STMT
 #endif /* !FRIBIDI_EMPTY_STMT */
 
-#if HAVE_STRINGIZE+0
+#ifdef HAVE_STRINGIZE
 # define STRINGIZE(symbol) #symbol
 #else /* !HAVE_STRINGIZE */
 #  error "No stringize operator available?"

bin/fribidi-benchmark.c
@@ -33,7 +33,7 @@
 #include <fribidi-deprecated.h>
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -41,16 +41,16 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
-#if HAVE_SYS_TIMES_H+0
+#ifdef HAVE_SYS_TIMES_H
 # include <sys/times.h>
 #endif
 #ifdef _WIN32
@@ -133,7 +133,7 @@ utime (
   else
       return 0.0;
 #else /* !_WIN32 */
-#if HAVE_SYS_TIMES_H+0
+#ifdef HAVE_SYS_TIMES_H
   struct tms tb;
   times (&tb);
   return 0.01 * tb.tms_utime;

bin/fribidi-main.c
@@ -31,13 +31,13 @@
 
 #include <fribidi.h>
 #include <fribidi-deprecated.h>
-#if FRIBIDI_CHARSETS+0
+#ifdef FRIBIDI_CHARSETS
 #else
 # include <fribidi-char-sets.h>
 #endif /* !FRIBIDI_CHARSETS */
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -45,14 +45,14 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if STDC_HEADERS && HAVE_MEMORY_H
 # else
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 

gen.tab/gen-arabic-shaping-tab.c
@@ -30,7 +30,7 @@
 #include <fribidi-unicode.h>
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -38,13 +38,13 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 

gen.tab/gen-bidi-type-tab.c
@@ -30,7 +30,7 @@
 #include <fribidi-unicode.h>
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -38,13 +38,13 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 

gen.tab/gen-brackets-tab.c
@@ -32,7 +32,7 @@
 #include <fribidi-unicode.h>
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -40,13 +40,13 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 

gen.tab/gen-brackets-type-tab.c
@@ -32,7 +32,7 @@
 #include <fribidi-unicode.h>
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -40,13 +40,13 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 

gen.tab/gen-joining-type-tab.c
@@ -30,7 +30,7 @@
 #include <fribidi-unicode.h>
 
 #include <stdio.h>
-#if STDC_HEADERS+0
+#ifdef STDC_HEADERS
 # include <stdlib.h>
 # include <stddef.h>
 #else
@@ -38,13 +38,13 @@
 #  include <stdlib.h>
 # endif
 #endif
-#if HAVE_STRING_H+0
+#ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
 # endif
 # include <string.h>
 #endif
-#if HAVE_STRINGS_H+0
+#ifdef HAVE_STRINGS_H
 # include <strings.h>
 #endif
 

commit 6f5757e4e2fa005b217a7833699b2ba5942a71db

Fix --disable-deprecated and --disable-charsets
I think I broke them in a7cf35b510dca601421ae36b14446bc800c44731.

Fixes https://github.com/fribidi/fribidi/issues/58

configure.ac
@@ -140,23 +140,19 @@ AC_ARG_ENABLE(deprecated,
               AC_HELP_STRING([--disable-deprecated],
                              [do not build deprecated functionality]))
 if test x$enable_deprecated = xno; then
-	FRIBIDI_NO_DEPRECATED=1
-else
-	FRIBIDI_NO_DEPRECATED=0
+	AC_DEFINE(FRIBIDI_NO_DEPRECATED, 1,
+		  [Don not build deprecated functionality])
 fi
-AC_SUBST(FRIBIDI_NO_DEPRECATED)
 
 # --disable-charsets
 AC_ARG_ENABLE(charsets,
               AC_HELP_STRING([--disable-charsets],
                              [exclude charset conversion routines from library]))
-if test x$enable_charsets = xno; then
-	FRIBIDI_CHARSETS=0
-else
-	FRIBIDI_CHARSETS=1
+if test x$enable_charsets != xno; then
+	AC_DEFINE(FRIBIDI_CHARSETS, 1,
+		  [Include charset conversion routines])
 fi
-AC_SUBST(FRIBIDI_CHARSETS)
-AM_CONDITIONAL(FRIBIDI_CHARSETS, test x$FRIBIDI_CHARSETS = x1)
+AM_CONDITIONAL(FRIBIDI_CHARSETS, test x$enable_charsets != xno)
 
 # --with[out]-glib
 AC_ARG_WITH(glib,

lib/common.h
@@ -127,7 +127,9 @@
 #endif /* !_GNU_SOURCE */
 
 /* We respect our own rules. */
-#define FRIBIDI_NO_DEPRECATED
+#ifndef FRIBIDI_NO_DEPRECATED
+#  define FRIBIDI_NO_DEPRECATED
+#endif /* !FRIBIDI_NO_DEPRECATED */
 
 
 #include "debug.h"

lib/fribidi-config.h.in
@@ -14,9 +14,6 @@
 #define FRIBIDI_INTERFACE_VERSION @FRIBIDI_INTERFACE_VERSION@
 #define FRIBIDI_INTERFACE_VERSION_STRING "@FRIBIDI_INTERFACE_VERSION@"
 
-/* Define to 1 if you want charset conversion codes in the library */
-#define FRIBIDI_CHARSETS @FRIBIDI_CHARSETS@
-
 /* The size of a `int', as computed by sizeof. */
 #define FRIBIDI_SIZEOF_INT @SIZEOF_INT@
 